c952be805d01a6abd5f85a6d31c8dd4887f7899d
Franz Miltz <franz@miltz.me>
2020-05-24 13:47:29 +0100

    Docs: Added comments for the Mapping class

--- src/main/java/Mapping.java
+++ src/main/java/Mapping.java
@@ -1,21 +1,32 @@
+/**
+ * A simple subsitution cipher that is its own inverse.
+ */
 public class Mapping {
+
+  /** The number of symbols in the mapping. */
   public static int SIZE = 26;
+
+  /** Which letter each position corresponds to. */
   private final Letter[] map = new Letter[SIZE];
 
-  public Mapping(Letter[] values) {
+  /**
+   * Given a permutation of Letters this constructs a mapping.
+   * @param values An array of values that corresponds to a valid mapping.
+   */
+  public Mapping(final Letter[] values) {
     if (values.length != SIZE) {
       throw new IllegalArgumentException(
           "Given letter array has invalid size.");
     }
-    boolean[] targetSet = new boolean[SIZE];
-    for (Letter v : values) {
+    final boolean[] targetSet = new boolean[SIZE];
+    for (final Letter v : values) {
       targetSet[v.value] = true;
       if (values[values[v.value].value].value !=
           v.value) { // Nice code... TODO: fix this
         throw new IllegalArgumentException("Mappings can only occur in pairs.");
       }
     }
-    for (boolean b : targetSet) {
+    for (final boolean b : targetSet) {
       if (!b) {
         throw new IllegalArgumentException(
             "Given letter array does not map to every letter.");
@@ -24,21 +35,44 @@ public class Mapping {
     System.arraycopy(values, 0, this.map, 0, SIZE);
   }
 
-  public Letter processPos(Letter position, Letter in) {
+  /**
+   * Given a position and an input, this returns the letter the input gets
+   * mapped to. The input and output are shifted to and from their actual
+   * locations to find the mapping that is appropriate.
+   *
+   * @param position The Letter that A gets translated to.
+   * @param in The Letter to be encrypted.
+   * @return The encrypted letter.
+   */
+  public Letter processPos(final Letter position, final Letter in) {
     return this.map[in.shift(position).value].unshift(position);
   }
 
-  public Letter process(Letter in) { return this.map[in.value]; }
+  /**
+   * Processes a single Letter.
+   * @param in The letter to be processed.
+   * @return The letter the input got mapped to.
+   */
+  public Letter process(final Letter in) { return this.map[in.value]; }
 
+  /**
+   * Converts the mapping back to an array of letters.
+   * @return The permutation representing this mapping.
+   */
   public Letter[] toLetterArray() {
-    var arr = new Letter[SIZE];
+    final var arr = new Letter[SIZE];
     for (int i = 0; i < SIZE; i++) {
       arr[i] = this.map[i];
     }
     return arr;
   }
 
-  public static Mapping fromString(String str) {
+  /**
+   * Creates a new mapping from a string.
+   * @param str The string representing the mapping.
+   * @return A new mapping.
+   */
+  public static Mapping fromString(final String str) {
     if (str.length() != SIZE) {
       throw new IllegalArgumentException();
     }
