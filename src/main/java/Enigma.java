92fe225cbe24a6b4b0ee11a6938444983a3c8fcc
Franz Miltz <franz@miltz.me>
2020-05-24 13:18:59 +0100

    Docs: Added comments for Enigma class

--- src/main/java/Enigma.java
+++ src/main/java/Enigma.java
@@ -1,14 +1,43 @@
+/**
+ * Handles the encryption using its current internal state.
+ */
 public class Enigma {
-  private Rotor[] rotors;
-  private Mapping reflector;
-  private Mapping translator;
+  /** The rotating encryption components of the enigma. May be empty. */
+  private final Rotor[] rotors;
 
-  public Enigma(Rotor[] rotors, Mapping translator, Mapping reflector) {
+  /**
+   * The non rotating encryption component at the end of the enigma. May not be
+   * null.
+   */
+  private final Mapping reflector;
+  /**
+   * The non rotating encryption component at the beginning of the enigma. May
+   * not be null.
+   */
+  private final Mapping translator;
+
+  /**
+   * To create an Enigma you need
+   *
+   * - a translator (which letter does each input key correspond to)
+   * - an array of rotors
+   * - a reflector (in between the forward and backward pass through the rotors)
+   * @param rotors rotors used in the enigma
+   * @param translator a simple mapping
+   * @param reflector a simple mapping
+   */
+  public Enigma(final Rotor[] rotors, final Mapping translator,
+                final Mapping reflector) {
     this.rotors = rotors;
     this.reflector = reflector;
     this.translator = translator;
   }
-
+  /**
+   * Given a letter, this changes the internal state accordingly and returns the
+   * encrypted letter.
+   * @param in A single letter.
+   * @return A single letter.
+   */
   public Letter process(Letter in) {
     in = this.translator.forward(in);
     for (int i = 0; i < this.rotors.length; i++) {
@@ -22,24 +51,41 @@ public class Enigma {
     return in;
   }
 
+  /**
+   * Returns an array of letters corresponding to the internal rotors that show
+   * which letter is at the top for each one.
+   * @return Letters that are at the top.
+   */
   public Letter[] getTopPositions() {
-    var positions = new Letter[this.rotors.length];
+    final var positions = new Letter[this.rotors.length];
     for (int i = 0; i < positions.length; i++) {
       positions[i] = this.rotors[i].getPosition();
     }
     return positions;
   }
 
-  public String processString(String in) {
-    var letters = Letter.arrayFromString(in);
+  /**
+   * Processes an entire String to make encryption more convenient. Don't use
+   * this if you want to track the internal state.
+   * @param in
+   * @return
+   */
+  public String processString(final String in) {
+    final var letters = Letter.arrayFromString(in);
     for (int i = 0; i < letters.length; i++) {
       letters[i] = this.process(letters[i]);
     }
     return Letter.arrayToString(letters);
   }
 
+  /**
+   * Returns the mappings of all the components of the enigma. The first element
+   * is the translator (plug board), then there are all the rotors and finally
+   * you get the reflectors configuration.
+   * @return An array of all the Mappings.
+   */
   public Mapping[] getMappings() {
-    var mappings = new Mapping[2 + this.rotors.length];
+    final var mappings = new Mapping[2 + this.rotors.length];
     mappings[0] = this.translator;
     for (int i = 0; i < this.rotors.length; i++) {
       mappings[i + 1] = this.rotors[i].getMapping();
